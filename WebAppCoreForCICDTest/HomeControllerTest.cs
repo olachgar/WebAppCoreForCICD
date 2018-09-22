b9c9183e0da4a6bd2315c6433eb0dca57fe24e3e
Omar LACHGAR <olachgar@gmail.com>
2018-09-22 12:40:22 +0200

    Adding an empty xUnit project to implement unit testing'

---
+++ WebAppCoreForCICDTest/HomeControllerTest.cs
@@ -0,0 +1,37 @@
+using System;
+using Xunit;
+
+namespace WebAppCoreForCICDTest
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void IndexTest()
+        {
+
+        }
+
+
+        [Fact]
+        public void AboutTest()
+        {
+
+        }
+
+
+        [Fact]
+        public void ContactTest()
+        {
+
+        }
+
+
+        [Fact]
+        public void ErrorTest()
+        {
+
+        }
+
+        
+    }
+}
---
+++ WebAppCoreForCICDTest/HomeControllerTests.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+
+namespace WebAppCoreForCICDTest
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void IndexTest()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+
+
+        [Fact]
+        public void AboutTest()
+        {
+
+        }
+
+
+        [Fact]
+        public void ContactTest()
+        {
+
+        }
+
+
+        [Fact]
+        public void ErrorTest()
+        {
+
+        }
+
+        
+    }
+}
